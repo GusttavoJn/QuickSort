cd5a77effe421e83b87beea2f9cca92d10965237
GusttavoJn <juniiogusttavo1770@gmail.com>
2024-10-25 23:06:38 -0300

    Initial commit

---
+++ src/QuickSort.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class QuickSort {
+
+    public static void quicksort(int[] v) {
+        quicksort(v, 0, v.length -1);
+    }
+
+    public static void quicksort(int[] v, int i, int f) {
+        if (i < f) {
+            int p = separacao(v, i, f);
+            quicksort(v, i, p -1);
+            quicksort(v, p + 1, f);
+        }
+    }
+    public static int separacao(int[] v, int i, int f) {
+        int pivor = v[i];
+        int e = i;
+        int d = f;
+
+        while (e <= d) {
+            while (e <= f && v[e] < pivor) e++;
+            while (d >= i && v[d] > pivor) d--;
+            
+            if (e <= d) {
+                int aux = v[e];
+                v[e] = v[d];
+                v[d] = aux;
+                e++;
+                d--;
+            }
+        }
+        return e;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        int[] vetor = {1, 3, 6, 2, 4, 5};
+
+        quicksort(vetor);
+
+        System.out.println(Arrays.toString(vetor).getClass());
+    }
+}
